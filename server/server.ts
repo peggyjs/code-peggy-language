954651ce9040caaecc665a665a78dd0ef1b1b267
Joe Hildebrand <joe-github@cursive.net>
2024-10-12 18:05:23 -0600

    Fixes #46
    Wait for parsing to finish before updating outline.
    Depends on #53.

--- server/server.ts
+++ server/server.ts
@@ -23,6 +23,7 @@ import {
 } from "vscode-languageserver/node";
 import { Position, TextDocument } from "vscode-languageserver-textdocument";
 import type { SourceNode } from "source-map-generator";
+import { WatchMap } from "./watchMap";
 import { debounce } from "../common/debounce";
 
 function getSession(
@@ -35,10 +36,7 @@ function getSession(
   ast.code = session as unknown as SourceNode;
 }
 
-interface AstCache {
-  [uri: string]: any;
-}
-const AST: AstCache = {};
+const AST = new WatchMap<string, any>();
 const WORD_RE = /[^\s{}[\]()`~!@#%^&*+\-=|\\;:'",./<>?]+/g;
 const PASSES: peggy.compiler.Stages = {
   prepare: peggy.compiler.passes.prepare,
@@ -158,7 +156,7 @@ const validateTextDocument = debounce((doc: TextDocument): void => {
       { grammarSource: doc.uri, output: "source-and-map" }
     ) as unknown as peggy.Session;
     addProblemDiagnostics(session.problems, diagnostics);
-    AST[doc.uri] = ast;
+    AST.set(doc.uri, ast);
   } catch (error) {
     if (error instanceof peggy.GrammarError) {
       addProblemDiagnostics(error.problems, diagnostics);
@@ -219,172 +217,186 @@ function ruleNameRange(name: string, ruleRange: Range): Range {
   };
 }
 
-connection.onCompletion((pos: TextDocumentPositionParams): CompletionItem[] => {
-  const docAST = AST[pos.textDocument.uri];
-  if (!docAST || (docAST.rules.length === 0)) {
-    return null;
-  }
-  const document = documents.get(pos.textDocument.uri);
-  if (!document) {
-    return null;
-  }
-  const word = getWordAtPosition(document, pos.position);
-  if (word === "") {
-    return null;
-  }
-
-  return docAST.rules.filter(
-    (r: any) => r.name.startsWith(word)
-  ).map((r: any) => ({
-    label: r.name,
-  }));
-});
-
-connection.onDefinition((pos: TextDocumentPositionParams): LocationLink[] => {
-  const docAST = AST[pos.textDocument.uri];
-  if (!docAST || (docAST.rules.length === 0)) {
-    return null;
-  }
-  const document = documents.get(pos.textDocument.uri);
-  if (!document) {
-    return null;
-  }
-  const word = getWordAtPosition(document, pos.position);
-  if (word === "") {
-    return null;
-  }
-
-  const rule = docAST.rules.find((r: any) => r.name === word);
-  if (!rule) {
-    return null;
-  }
-  const targetRange = peggyLoc_to_vscodeRange(rule.location);
-  const targetSelectionRange = ruleNameRange(rule.name, targetRange);
-
-  return [
-    {
-      targetUri: pos.textDocument.uri,
-      targetRange,
-      targetSelectionRange,
-    },
-  ];
-});
-
-connection.onReferences((pos: TextDocumentPositionParams): Location[] => {
-  const docAST = AST[pos.textDocument.uri];
-  if (!docAST || (docAST.rules.length === 0)) {
-    return null;
-  }
-  const document = documents.get(pos.textDocument.uri);
-  if (!document) {
-    return null;
-  }
-  const word = getWordAtPosition(document, pos.position);
-  if (word === "") {
-    return null;
-  }
-  const results: Location[] = [];
-  const visit = peggy.compiler.visitor.build({
-    rule_ref(node: any): void {
-      if (node.name !== word) { return; }
-      results.push({
-        uri: pos.textDocument.uri,
-        range: peggyLoc_to_vscodeRange(node.location),
-      });
-    },
-  });
-  visit(docAST);
-
-  return results;
-});
-
-connection.onRenameRequest((pos: RenameParams): WorkspaceEdit => {
-  const docAST = AST[pos.textDocument.uri];
-  if (!docAST || (docAST.rules.length === 0)) {
-    return null;
-  }
-  const document = documents.get(pos.textDocument.uri);
-  if (!document) {
-    return null;
-  }
-  const word = getWordAtPosition(document, pos.position);
-  if (word === "") {
-    return null;
-  }
-
-  const edits: TextEdit[] = [];
-  const visit = peggy.compiler.visitor.build({
-    rule_ref(node: any): void {
-      if (node.name !== word) { return; }
-      edits.push({
-        newText: pos.newName,
-        range: peggyLoc_to_vscodeRange(node.location),
-      });
-    },
-
-    rule(node: any): void {
-      visit(node.expression);
-      if (node.name !== word) { return; }
-      edits.push({
-        newText: pos.newName,
-        range: ruleNameRange(node.name, peggyLoc_to_vscodeRange(node.location)),
-      });
-    },
-  });
-  visit(docAST);
-
-  return {
-    changes: {
-      [pos.textDocument.uri]: edits,
-    },
-  };
-});
-
-connection.onDocumentSymbol((pos: DocumentSymbolParams): DocumentSymbol[] => {
-  const docAST = AST[pos.textDocument.uri];
-  if (!docAST) {
-    return null;
-  }
-
-  const symbols = docAST.rules.map((r: any) => {
-    const range = peggyLoc_to_vscodeRange(r.location);
-    const ret: DocumentSymbol = {
-      name: r.name,
-      kind: SymbolKind.Function,
-      range,
-      selectionRange: ruleNameRange(r.name, range),
-    };
-    if (r.expression.type === "named") {
-      ret.detail = r.expression.name;
+connection.onCompletion(
+  async(pos: TextDocumentPositionParams): Promise<CompletionItem[]> => {
+    const docAST = await AST.waitFor(pos.textDocument.uri);
+    if (!docAST || (docAST.rules.length === 0)) {
+      return null;
+    }
+    const document = documents.get(pos.textDocument.uri);
+    if (!document) {
+      return null;
+    }
+    const word = getWordAtPosition(document, pos.position);
+    if (word === "") {
+      return null;
     }
 
-    return ret;
-  });
-  if (docAST.initializer) {
-    const range = peggyLoc_to_vscodeRange(docAST.initializer.location);
-    symbols.unshift({
-      name: "{Per-parse initializer}",
-      kind: SymbolKind.Constructor,
-      range,
-      selectionRange: ruleNameRange("{", range),
-    });
+    return docAST.rules.filter(
+      (r: any) => r.name.startsWith(word)
+    ).map((r: any) => ({
+      label: r.name,
+    }));
   }
-  if (docAST.topLevelInitializer) {
-    const range = peggyLoc_to_vscodeRange(docAST.topLevelInitializer.location);
-    symbols.unshift({
-      name: "{{Global initializer}}",
-      kind: SymbolKind.Constructor,
-      range,
-      selectionRange: ruleNameRange("{{", range),
-    });
-  }
+);
 
-  return symbols;
-});
+connection.onDefinition(
+  async(pos: TextDocumentPositionParams): Promise<LocationLink[]> => {
+    const docAST = await AST.waitFor(pos.textDocument.uri);
+    if (!docAST || (docAST.rules.length === 0)) {
+      return null;
+    }
+    const document = documents.get(pos.textDocument.uri);
+    if (!document) {
+      return null;
+    }
+    const word = getWordAtPosition(document, pos.position);
+    if (word === "") {
+      return null;
+    }
+
+    const rule = docAST.rules.find((r: any) => r.name === word);
+    if (!rule) {
+      return null;
+    }
+    const targetRange = peggyLoc_to_vscodeRange(rule.location);
+    const targetSelectionRange = ruleNameRange(rule.name, targetRange);
+
+    return [
+      {
+        targetUri: pos.textDocument.uri,
+        targetRange,
+        targetSelectionRange,
+      },
+    ];
+  }
+);
+
+connection.onReferences(
+  async(pos: TextDocumentPositionParams): Promise<Location[]> => {
+    const docAST = await AST.get(pos.textDocument.uri);
+    if (!docAST || (docAST.rules.length === 0)) {
+      return null;
+    }
+    const document = documents.get(pos.textDocument.uri);
+    if (!document) {
+      return null;
+    }
+    const word = getWordAtPosition(document, pos.position);
+    if (word === "") {
+      return null;
+    }
+    const results: Location[] = [];
+    const visit = peggy.compiler.visitor.build({
+      rule_ref(node: any): void {
+        if (node.name !== word) { return; }
+        results.push({
+          uri: pos.textDocument.uri,
+          range: peggyLoc_to_vscodeRange(node.location),
+        });
+      },
+    });
+    visit(docAST);
+
+    return results;
+  }
+);
+
+connection.onRenameRequest(
+  async(pos: RenameParams): Promise<WorkspaceEdit> => {
+    const docAST = await AST.get(pos.textDocument.uri);
+    if (!docAST || (docAST.rules.length === 0)) {
+      return null;
+    }
+    const document = documents.get(pos.textDocument.uri);
+    if (!document) {
+      return null;
+    }
+    const word = getWordAtPosition(document, pos.position);
+    if (word === "") {
+      return null;
+    }
+
+    const edits: TextEdit[] = [];
+    const visit = peggy.compiler.visitor.build({
+      rule_ref(node: any): void {
+        if (node.name !== word) { return; }
+        edits.push({
+          newText: pos.newName,
+          range: peggyLoc_to_vscodeRange(node.location),
+        });
+      },
+
+      rule(node: any): void {
+        visit(node.expression);
+        if (node.name !== word) { return; }
+        edits.push({
+          newText: pos.newName,
+          range: ruleNameRange(
+            node.name,
+            peggyLoc_to_vscodeRange(node.location)
+          ),
+        });
+      },
+    });
+    visit(docAST);
+
+    return {
+      changes: {
+        [pos.textDocument.uri]: edits,
+      },
+    };
+  }
+);
+
+connection.onDocumentSymbol(
+  async(pos: DocumentSymbolParams): Promise<DocumentSymbol[]> => {
+    const docAST = await AST.waitFor(pos.textDocument.uri);
+    if (!docAST) {
+      return null;
+    }
+
+    const symbols = docAST.rules.map((r: any) => {
+      const range = peggyLoc_to_vscodeRange(r.location);
+      const ret: DocumentSymbol = {
+        name: r.name,
+        kind: SymbolKind.Function,
+        range,
+        selectionRange: ruleNameRange(r.name, range),
+      };
+      if (r.expression.type === "named") {
+        ret.detail = r.expression.name;
+      }
+
+      return ret;
+    });
+    if (docAST.initializer) {
+      const range = peggyLoc_to_vscodeRange(docAST.initializer.location);
+      symbols.unshift({
+        name: "{Per-parse initializer}",
+        kind: SymbolKind.Constructor,
+        range,
+        selectionRange: ruleNameRange("{", range),
+      });
+    }
+    if (docAST.topLevelInitializer) {
+      const range = peggyLoc_to_vscodeRange(
+        docAST.topLevelInitializer.location
+      );
+      symbols.unshift({
+        name: "{{Global initializer}}",
+        kind: SymbolKind.Constructor,
+        range,
+        selectionRange: ruleNameRange("{{", range),
+      });
+    }
+
+    return symbols;
+  }
+);
 
 documents.onDidClose(change => {
-  // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-  delete AST[change.document.uri.toString()];
+  AST.delete(change.document.uri.toString());
 });
 
 documents.onDidChangeContent(change => {
---
+++ server/watchMap.ts
@@ -0,0 +1,49 @@
+export type Notification<K, V> = (key: K, value: V) => void;
+
+// Light pub-sub over a map
+export class WatchMap<K, V> extends Map<K, V> {
+  #pending = new Map<K, Notification<K, V>[]>();
+
+  public set(key: K, value: V): this {
+    super.set(key, value);
+    const watchers = this.#pending.get(key);
+    if (watchers) {
+      this.#pending.delete(key);
+      for (const w of watchers) {
+        w.call(this, key, value);
+      }
+    }
+    return this;
+  }
+
+  public delete(key: K): boolean {
+    if (super.delete(key)) {
+      // This should never delete anything
+      this.#pending.delete(key);
+      return true;
+    }
+    return false;
+  }
+
+  public waitFor(key: K): Promise<V> {
+    return new Promise(resolve => {
+      if (this.has(key)) {
+        resolve(this.get(key));
+      } else {
+        this.#watch(key, (_k, v) => {
+          resolve(v);
+        });
+      }
+    });
+  }
+
+  #watch(key: K, cb: Notification<K, V>): this {
+    let watchers = this.#pending.get(key);
+    if (!watchers) {
+      watchers = [];
+      this.#pending.set(key, watchers);
+    }
+    watchers.push(cb);
+    return this;
+  }
+}
